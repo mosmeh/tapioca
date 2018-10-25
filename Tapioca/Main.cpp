eaae3778b16734fa9f0eca45eedc5f28e2723a16
mosm <airman515@gmail.com>
2018-10-25 14:20:54 +0900

    WIP

--- Tapioca/Main.cpp
+++ Tapioca/Main.cpp
@@ -10,14 +10,6 @@ enum class Scene {
     GameOver
 };
 
-struct Data {
-    Font font = Font(30);
-    int score = 0;
-    int highScore = 0;
-};
-
-using App = SceneManager<Scene, Data>;
-
 class Animation {
 public:
     Animation(std::vector<FilePath> textures, double intervals, bool looped = true, bool immediatelyStarted = true) :
@@ -71,7 +63,7 @@ private:
     bool looped;
     std::vector<TextureAsset> texAssets;
     Timer timer;
-    int idx = 0;
+    size_t idx = 0;
     bool started = false;
 };
 
@@ -88,6 +80,7 @@ public:
 
     void draw() const {
         floorRect.draw(Color(123, 58, 21));
+        floorRect.top().draw(5.0, Palette::Black);
         sunRect(sunAnim.get()).draw();
     }
 
@@ -179,7 +172,7 @@ public:
         explosionAnim.update();
 
         if (explosionAnim.isFinished()) {
-            exploded = true;
+            destroyed = true;
             return;
         }
         if (explosionAnim.isStarted()) {
@@ -208,8 +201,8 @@ public:
         rect(tex).draw();
     }
 
-    bool hit() const {
-        return exploded;
+    bool isDestroyed() const {
+        return destroyed;
     }
 
 private:
@@ -217,7 +210,7 @@ private:
     static const double size;
     RectF rect;
     Vec2 velocity;
-    bool exploded = false;
+    bool destroyed = false;
     Animation explosionAnim;
 };
 
@@ -227,8 +220,8 @@ const double Egg::size = 50.0;
 class Player {
 public:
     Player() :
-        rect(50, Window::Height() - floorHeight - 100, 100, 100),
-        eggLaunchTimer(1.0, false),
+        rect(100, Window::Height() - floorHeight - size.y, size),
+        eggLaunchTimer(0.5, false),
         restingAnim({ U"stop1", U"stop2" }, 0.3),
         throwingAnim({ U"throw1" }, 0.2, false, false) {
         eggLaunchTimer.set(0s);
@@ -242,7 +235,7 @@ public:
         }
         if (egg) {
             egg->update(blocks);
-            if (egg->hit()) {
+            if (egg->isDestroyed()) {
                 egg = none;
             }
         }
@@ -314,7 +307,9 @@ public:
             egg->draw();
         }
         const auto& tex = (!throwingAnim.isStarted() || throwingAnim.isFinished()) ? restingAnim.get() : throwingAnim.get();
-        rect(tex.mirrored(facingRight)).draw();
+        constexpr double heightInTexels = 315.0;
+        const auto stex = tex.mirrored(facingRight).scaled(rect.h / heightInTexels);
+        stex.drawAt(rect.bottomCenter() - Vec2(0.0, stex.size.y / 2.0));
     }
 
     bool isDead() const {
@@ -322,6 +317,7 @@ public:
     }
 
 private:
+    static const Size size;
     static const double speed;
     double vy = 0.0;
     RectF rect;
@@ -333,23 +329,60 @@ private:
     Animation restingAnim, throwingAnim;
 };
 
-const double Player::speed = 10;
+const Size Player::size = Size(40, 70);
+
+const double Player::speed = 8;
+
+struct Data {
+    Font font = Font(28);
+    int score = 0;
+    int highScore = 0;
+    Stage stage;
+    Player player;
+    std::vector<Block> blocks;
+};
+
+using App = SceneManager<Scene, Data>;
 
 class Title : public App::Scene {
 public:
-    Title(const InitData& init) : IScene(init) {}
+    Title(const InitData& init) : IScene(init) {
+        const auto tex = TextureAsset(U"title");
+        titleTex = tex.scaled(static_cast<double>(Window::Width()) / tex.width());
+    }
+
+    void update() override {
+        if (KeyZ.down()) {
+            changeScene(Scene::Playing, 0, false);
+        }
+    }
+
+    void draw() const override {
+        getData().stage.draw();
+        getData().player.draw();
+        getData().font(U"SCORE: ", getData().score, U" HIGHSCORE: ", getData().highScore)
+            .draw(Vec2(0, 0), Palette::Black);
+        titleTex.drawAt(Window::Center());
+    }
+
+private:
+    TextureRegion titleTex;
 };
 
 class Playing : public App::Scene {
 public:
     Playing(const InitData& init) : IScene(init) {
+        getData().score = 0;
+        getData().player = Player();
+        getData().blocks = {};
         blockFallSW.start();
     }
 
     void update() override {
-        stage.update();
+        getData().stage.update();
 
-        constexpr int blockFallIntervalms = 1000;
+        auto& blocks = getData().blocks;
+        constexpr int blockFallIntervalms = 500;
         if (blockFallSW.ms() > blockFallIntervalms) {
             blocks.emplace_back(
                 Random(0, numBlocksX - 1) * Window::Width() /
@@ -360,42 +393,67 @@ public:
             block.update(blocks);
             if (block.isTouchingTop()) {
                 changeScene(Scene::GameOver, 0, false);
+                return;
             }
         }
+
+        getData().player.update(getData().blocks);
+        if (getData().player.isDead()) {
+            changeScene(Scene::GameOver, 0, false);
+            return;
+        }
+
         const auto numBlocks = blocks.size();
         blocks.erase(
             std::remove_if(blocks.begin(), blocks.end(),
                 [](const auto& block) { return block.isDestroyed(); }),
             blocks.end());
-        getData().score += 10 * std::max(0, static_cast<int>(numBlocks - blocks.size()));
-        getData().highScore = std::max(getData().score, getData().highScore);
-
-        player.update(blocks);
-        if (player.isDead()) {
-            changeScene(Scene::GameOver, 0, false);
+        if (numBlocks > blocks.size()) {
+            getData().score += 10;
+            getData().highScore = std::max(getData().score, getData().highScore);
         }
     }
 
     void draw() const override {
-        stage.draw();
-        for (auto& block : blocks) {
+        getData().stage.draw();
+        for (auto& block : getData().blocks) {
             block.draw();
         }
-        player.draw();
+        getData().player.draw();
         getData().font(U"SCORE: ", getData().score, U" HIGHSCORE: ", getData().highScore)
             .draw(Vec2(0, 0), Palette::Black);
     }
 
 private:
-    Stage stage;
-    Player player;
-    std::vector<Block> blocks;
     Stopwatch blockFallSW;
 };
 
 class GameOver : public App::Scene {
 public:
-    GameOver(const InitData& init) : IScene(init) {}
+    GameOver(const InitData& init) : IScene(init) {
+        const auto tex = TextureAsset(U"gameover");
+        gameOverTex = tex.scaled(static_cast<double>(Window::Width()) / tex.width());
+    }
+
+    void update() override {
+        if (KeyR.down()) {
+            changeScene(Scene::Playing, 0, false);
+        }
+    }
+
+    void draw() const override {
+        getData().stage.draw();
+        for (auto& block : getData().blocks) {
+            block.draw();
+        }
+        getData().player.draw();
+        getData().font(U"SCORE: ", getData().score, U" HIGHSCORE: ", getData().highScore)
+            .draw(Vec2(0, 0), Palette::Black);
+        gameOverTex.drawAt(Window::Center());
+    }
+
+private:
+    TextureRegion gameOverTex;
 };
 
 void Main() {
@@ -407,23 +465,37 @@ void Main() {
     TextureAsset::Register(U"block", U"imgs/block.png");
     TextureAsset::Register(U"boom1", U"imgs/boom1.png");
     TextureAsset::Register(U"boom2", U"imgs/boom2.png");
+    TextureAsset::Register(U"gameover", U"imgs/gameover.png");
     TextureAsset::Register(U"stop1", U"imgs/stop1.png");
     TextureAsset::Register(U"stop2", U"imgs/stop2.png");
     TextureAsset::Register(U"sun1", U"imgs/sun1.png");
     TextureAsset::Register(U"sun2", U"imgs/sun2.png");
     TextureAsset::Register(U"tamago", U"imgs/tamago.png");
     TextureAsset::Register(U"throw1", U"imgs/throw1.png");
+    TextureAsset::Register(U"title", U"imgs/title.png");
 
-    App mgr;
+    const auto data = std::make_shared<Data>();
+
+    const auto scoreFile = U"score";
+    if (FileSystem::Exists(scoreFile)) {
+        BinaryReader reader(scoreFile);
+        if (!reader.read(data->highScore)) {
+            data->highScore = 0;
+        }
+    }
+
+    App mgr(data);
     mgr.add<Title>(Scene::Title)
         .add<Playing>(Scene::Playing)
         .add<GameOver>(Scene::GameOver);
-    mgr.init(Scene::Title);
-    mgr.changeScene(Scene::Playing, 0, false);
+    mgr.changeScene(Scene::Title, 0, false);
 
     while (System::Update()) {
         if (!mgr.update()) {
             break;
         }
     }
+
+    BinaryWriter writer(scoreFile);
+    writer.write(&data->highScore, sizeof(data->highScore));
 }
